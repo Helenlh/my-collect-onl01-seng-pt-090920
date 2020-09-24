18ec4e838fa8876507f0a9367c0b4eb31dd3d525
HELEN HARMAN <hlharman@bellsouth.net>
2020-09-24 22:51:01 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,65 +1,70 @@
 
 my_collect(languages)
   languages =  ['ruby', 'javascript', 'python', 'objective-c'] 
-  students =  ['Tim Jones', 'Tom Smith', 'Sophie Johnson', 'Antoin Miller'] 
-
-  
-    empty_array = []
+  my_collect(array) do|sample|
+      empty_array = []
     counter = 0
-    if block_given 
-    while counter < empty_array.length
-      my_collect(empty_array) do |x|
-        counter += 1
-      else 
-        puts "No block was given."
-      end
+    if block_given?
+      while counter < empty_array.length
+      array.collect(empty_array) do |x|
+      counter += 1 
+      end 
       counter 
-  end
+    end 
+    else 
+      puts "No block was given."
+  end 
 end 
 
-  it "yields the correct element from a given collection, in this case languages
-   
-   
-    my_collect(languages) 
-  if block_given 
-  
-  
-  yield language[i]
-  languages << language 
-    end
-  else puts "No block was given."
+
+
+   my_collect(languages) do |language|
+     counter = 0 
+     if block_given?
+       while counter < language.length 
+       language.collect do |language|
+       yield language[i]  
+      languages<< language
+       counter +=1 
+     end 
+   else 
+      puts "No block was given."
+    end 
+    languages 
   end
 
+
+
+
+my_collect(languages)
+while counter < languages.length 
        language.upcase
-      
-    end).to eq(["RUBY", "JAVASCRIPT", "PYTHON", "OBJECTIVE-C"])
+      end 
+end 
+
+
+
+
+students = ['Tim Jones', 'Tom Smith', 'Sophie Johnson', 'Antoin Miller'] 
+
+ my_collect(students) do |names|
+   counter = 0 
+     if block_given?
+       while counter < students.length 
+       students.collect do |names|
+       yield names[i]  
+       students << names 
+       counter +=1 
+       end 
+     students 
+      else 
+      puts "No block was given."
+    end 
   end
-
-  it 'does not modify the original collection' do
-    my_collect(languages) do |language|
-      language.upcase
-    end
-    expect(languages).to eq(['ruby', 'javascript', 'python', 'objective-c'])
-  end
-
-  it "yields the correct element from the given collection, in this case students" do
-    my_collect(students) do |student|
-      expect(student).to_not eq(nil)
-    end
-  end
-
-  it "returns a new collection of appropriately modified elements, in this case student first names" do
-    expect(my_collect(students) do |student|
-      student.split(" ").first
-    end).to eq(["Tim", "Tom", "Sophie", "Antoin"])
-  end
-
-  it 'does not modify the original collection' do
-    my_collect(students) do |student|
-      student.split(" ").first
-    end
-    expect(students).to eq(['Tim Jones', 'Tom Smith', 'Sophie Johnson', 'Antoin Miller'])
-  end
-end
-
-
+  
+  
+  
+  
+  my_collect(students)
+  names.split(" ").join 
+end 
